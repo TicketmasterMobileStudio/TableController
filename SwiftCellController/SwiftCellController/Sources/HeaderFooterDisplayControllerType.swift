cb624b2738040f18b23b97b33fb4d7a0dc25b0af
Carmen Cerino <carmen.cerino@ticketmaster.com>
2016-07-21 08:22:18 -0400

    Adding a protocol to describe an object to handle footer/header config

---
+++ SwiftCellController/SwiftCellController/Sources/HeaderFooterDisplayControllerType.swift
@@ -0,0 +1,31 @@
+//
+//  HeaderFooterDisplayControllerType.swift
+//  SwiftCellController
+//
+//  Created by Carmen Cerino on 7/21/16.
+//  Copyright © 2016 Duncan Lewis. All rights reserved.
+//
+
+import Foundation
+
+/// A `HeaderFooterDisplayControllerType` describes something that handles the display of
+/// of a single `UIView` that represents a `UITableView` header or footer.
+public protocol HeaderFooterDisplayControllerType {
+    
+    // MARK: Sizing
+    var estimatedHeight: CGFloat { get }
+    var height: CGFloat { get }
+    
+    // MARK: Configuration
+    var type: TableReusableViewType { get }
+    func configureView(view: UIView)
+    func willDisplayView(view: UIView)
+    func didDisplayView(view: UIView)
+}
+
+public extension HeaderFooterDisplayControllerType {
+    var height: CGFloat { return UITableViewAutomaticDimension }
+    var estimatedHeight: CGFloat { return UITableViewAutomaticDimension }
+    func willDisplayView(view: UIView) { }
+    func didDisplayView(view: UIView) { }
+}
