9e8f7480d8ec96124f24323529645ad1b5013a9e
Duncan Lewis <duncan.lewis@ticketmaster.com>
2017-07-21 15:47:53 -0400

    Fix sample project building on devices

---
+++ ExampleProject/TableController.framework/Headers/TableController.h
@@ -0,0 +1,17 @@
+//
+//  TableController.h
+//  TableController
+//
+//  Created by Duncan Lewis on 7/18/16.
+//  Copyright © 2016 Duncan Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableController.
+FOUNDATION_EXPORT double TableControllerVersionNumber;
+
+//! Project version string for TableController.
+FOUNDATION_EXPORT const unsigned char TableControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableController/PublicHeader.h>
